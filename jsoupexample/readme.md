44bf18130fdf8c62452bea92d9a670faea80bf1d
fissha <superfissha@hotmail.com>
2020-03-17 10:09:33 +0100

    Add files via upload

---
+++ jsoupexample/readme.md
@@ -0,0 +1,3 @@
+# JSOUP PROJECT EXAMPLE
+
+With this example, you can see how to list all URLs from a target web and save it for any purpose.
---
+++ jsoupexample/src/main/java/com/urbanojvr/jsupexample/ParserEngine.java
@@ -0,0 +1,59 @@
+package com.urbanojvr.jsupexample;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ParserEngine {
+
+    private String baseUrl;
+    private ArrayList<String> urlList;
+
+    public ParserEngine(String baseUrl){
+        this.baseUrl = baseUrl;
+        this.urlList = new ArrayList<String>();
+    }
+
+    public void crawl(String url) throws IOException {
+        Document doc = Jsoup.connect(url).ignoreContentType(true).get();
+        Elements links = doc.select("a[href]");
+
+        for (Element link : links) {
+            String actualUrl = link.attr("abs:href");
+
+            if (!urlList.contains(actualUrl) & actualUrl.startsWith(baseUrl)){
+                print(" * a: <%s>  (%s)", actualUrl, trim(link.text(), 35));
+                urlList.add(actualUrl);
+                crawl(actualUrl);
+            }
+        }
+    }
+
+    private static void print(String msg, Object... args) {
+        System.out.println(String.format(msg, args));
+    }
+
+    private static String trim(String s, int width) {
+        if (s.length() > width)
+            return s.substring(0, width-1) + ".";
+        else
+            return s;
+    }
+
+    public String getBaseUrl(){
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String url){
+        baseUrl = url;
+    }
+
+    public ArrayList<String> getUrlList(){
+        return urlList;
+    }
+
+}
